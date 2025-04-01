3a0beec229b0d9fd7543f486bb4e779a25b5d23b
hieunm2 <hieunm2@vnext.vn>
2025-04-01 10:58:47 +0700

    init commit

---
+++ src/main/java/hieu/loadbalancerribbon/LoadBalancerRibbonApplication.java
@@ -0,0 +1,13 @@
+package hieu.loadbalancerribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancerRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadBalancerRibbonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/loadbalancerribbon/LoadBalancerRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.loadbalancerribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancerRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
