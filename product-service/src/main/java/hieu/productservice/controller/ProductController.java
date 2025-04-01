42f3c4b3a9b4b939d84f6bcd6281a30018b347ef
hieunm2 <hieunm2@vnext.vn>
2025-04-01 14:58:44 +0700

    circuit breaker ( still not working )

--- product-service/src/main/java/hieu/productservice/controller/ProductController.java
+++ product-service/src/main/java/hieu/productservice/controller/ProductController.java
@@ -2,6 +2,7 @@ package hieu.productservice.controller;
 
 
 import hieu.productservice.entity.Product;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     @GetMapping("/products/{id}")
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackGetProductById")
     public Product getProductById(@PathVariable Long id) {
         log.info("Get product by id from server 1 ");
         // For simplicity, returning a hardcoded product. In a real application, you'd query the database.
+        if (id == 4) {
+            throw new RuntimeException("Simulated error"); // For demonstration purposes, simulating a service failure.
+        }
         return new Product(id, "Sample Product", 99.99);
+    }
+
+    public Product fallbackGetProductById(Long id, Throwable throwable) {
+        log.error("Fallback method");
+        return new Product(id, "Fallback Product", 0);
     }
 }
--- product-service2/src/main/java/hieu/productservice/controller/ProductController.java
+++ product-service2/src/main/java/hieu/productservice/controller/ProductController.java
@@ -2,10 +2,10 @@ package hieu.productservice.controller;
 
 
 import hieu.productservice.entity.Product;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     @GetMapping("/products/{id}")
+    @CircuitBreaker(name = "productService", fallbackMethod = "fallbackGetProductById")
     public Product getProductById(@PathVariable Long id) {
         log.info("Get product by id from server 2 ");
+        if (id == 4) {
+            throw new RuntimeException("Simulated error"); // For demonstration purposes, simulating a service failure.
+        }
         // For simplicity, returning a hardcoded product. In a real application, you'd query the database.
         return new Product(id, "Sample Product", 99.99);
+    }
+
+    public Product fallbackGetProductById(Long id, Throwable throwable) {
+        log.error("Fallback method");
+        return new Product(id, "Fallback Product", 0);
     }
 }
